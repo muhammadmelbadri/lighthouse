c9ec41c6535190dbc96dc81bfdb111ea9f06ffc2
muhammadmelbadri <75115422+muhammadmelbadri@users.noreply.github.com>
2024-02-14 18:16:34 -0500

    Update script.js

--- script.js
+++ script.js
@@ -10,3 +10,20 @@ function addCategory() {
   document.getElementById('hoursSpent').value = '';
 }
 
+function updateYearInfo() {
+  const now = new Date();
+  const currentYear = now.getFullYear();
+  const endOfYear = new Date(currentYear, 11, 31, 23, 59, 59); // December 31st
+  const msPerDay = 24 * 60 * 60 * 1000; // number of milliseconds in a day
+  
+  const daysLeft = Math.ceil((endOfYear - now) / msPerDay);
+  const hoursLeft = daysLeft * 24;
+
+  document.getElementById('yearInfo').innerHTML = `
+    <div>${daysLeft} Days left in ${currentYear}</div>
+    <div>${hoursLeft} Hours left in ${currentYear}</div>
+  `;
+}
+
+// Call this function when the script loads
+updateYearInfo();
